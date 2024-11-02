ac37a92671a1541f2dddd78e67996b2ea0230f2d
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2024-11-02 19:58:24 +0200

    added some methods to the implementaion of cu class

--- cu.cpp
+++ cu.cpp
@@ -1,5 +1,23 @@
 #include "cu.h"
 #include "memory.h"
+#include <QString>
 
 cu::cu() {}
 
+void cu::load(int indexReg, int indexMem, Memory& mem, Register& reg){
+    Qstring valueToLoad = mem.getCell(indexMem);
+    reg.setCell(indexReg, valueToLoad);
+}
+
+void cu::load(int indexReg, QString val, Register& reg){
+    reg.setCell(indexReg, val);
+}
+
+void cu::move(int indexReg1, int indexReg2, Register& reg){
+    QString valueToMove = reg.getCell(indexReg1);
+    reg.setCell(indexReg2, valueToMove);
+}
+
+void cu::jump(int indexReg, int indexMem, Register& reg){
+    
+}
