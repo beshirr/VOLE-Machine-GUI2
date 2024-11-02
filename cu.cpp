b9ca679db2b6d34e6869089d6d18202fa5279ad2
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2024-11-02 21:32:10 +0200

    fixed some methods in cu implementaion

--- cu.cpp
+++ cu.cpp
@@ -14,23 +14,21 @@ void cu::load(int indexReg, QString val, Register& reg){
     reg.setCell(indexReg, val);
 }
 
-void cu::store(int indexReg, int indexMem, Memory& mem, Register& reg)
-{ 
+void cu::store(int indexReg, int indexMem, Memory& mem, Register& reg){
     Qstring valueToLoad = reg.getCell(indexReg); 
 
-    if (indexMem == "00") {
-        cout << valueToLoad<<endl;
+    if (to_string(indexMem) == "00") {
+        cout << valueToLoad << endl;
     }
     mem.setCell(indexMem, valueToLoad);
 }
-
-
 
 void cu::move(int indexReg1, int indexReg2, Register& reg){
     QString valueToMove = reg.getCell(indexReg1);
     reg.setCell(indexReg1, "00");
     reg.setCell(indexReg2, valueToMove);
 }
+
 void halt(){
     throw runtime_error("execution of halt instruction");
 }
