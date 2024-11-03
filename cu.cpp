a54f252cc8e9f0fec1e245b05f8ddfeefb2c1c3b
beshir <youssefahmedbeshir@gmail.com>
2024-11-03 17:07:33 +0200

    Fixing issues, jump modified

--- cu.cpp
+++ cu.cpp
@@ -1,41 +1,39 @@
 #include "cu.h"
-#include "memory.h"
-#include <QString>
-#include "cpu.h"
-#include <stdexcept>
-#include "alu.h"
 
-cu::cu() {}
+
+cu::cu() = default;
 
 void cu::load(const QString& indexReg, const QString& indexMem, Memory& mem, Register& reg){
-    QString valueToLoad = mem.getCell(ALU::hexToDec(indexMem));
-    reg.setCell(ALU::hexToDec(indexReg), valueToLoad);
+    QString valueToLoad = mem.getCell(indexMem.toInt(nullptr, 16));
+    reg.setCell(indexReg.toInt(nullptr, 16), valueToLoad);
 }
 
 void cu::load(const QString& indexReg, const QString& val, Register& reg){
-    reg.setCell(ALU::hexToDec(indexReg), val);
+    reg.setCell(indexReg.toInt(nullptr, 16), val);
 }
 
-void cu::store(int indexReg, const QString& indexMem, Memory& mem, Register& reg){
-    QString valueToStore = reg.getCell(indexReg);
+void cu::store(const QString& indexReg, const QString& indexMem, Memory& mem, Register& reg){
+    QString valueToStore = reg.getCell(indexReg.toInt(nullptr, 16));
 
     if (indexMem == "00") {
+        // TODO: SCREEN OUTPUT
     }
-    mem.setCell(ALU::hexToDec(indexMem), valueToStore);
+    mem.setCell(indexMem.toInt(nullptr, 16), valueToStore);
 }
 
 void cu::move(const QString& indexReg1, const QString& indexReg2, Register& reg){
-    QString valueToMove = reg.getCell(ALU::hexToDec(indexReg1));
-    reg.setCell(ALU::hexToDec(indexReg1), "00");
-    reg.setCell(ALU::hexToDec(indexReg2), valueToMove);
+    QString valueToMove = reg.getCell(indexReg1.toInt(nullptr, 16));
+    reg.setCell(indexReg1.toInt(nullptr, 16), "00");
+    reg.setCell(indexReg2.toInt(nullptr, 16), valueToMove);
 }
 
-void cu::jump(const QString& indexReg, int newProgramCounter, Register& reg)
+// TODO: WHEN CALLING THIS FUNCTION DO NOT FORGET TO UPDATE THE PROGRAM COUNTER m_programCounter = programCounter
+void cu::jump(const QString& indexReg, const QString& indexMem, Register& reg, Memory& mem, int programCounter)
 {
-    if (reg.getCell(ALU::hexToDec(indexReg)) == reg.getCell(0)) {
-        cpu::m_programCounter = newProgramCounter;
+    if (reg.getCell(indexReg.toInt(nullptr, 16)) == reg.getCell(0)) {
+        QString value = mem.getCell(indexMem.toInt(nullptr, 16));
+        mem.setCell(programCounter, value);
     }
-
 }
 
 
--- cu.h
+++ cu.h
@@ -3,15 +3,17 @@
 
 #include "memory.h"
 #include <QString>
+#include <stdexcept>
+#include "alu.h"
 
 class cu{
 public:
     cu();
     static void load(const QString& indexReg, const QString& indexMem, Memory& mem, Register& reg);
     static void load(const QString& indexReg, const QString& val, Register& reg);
-    static void store(int indexReg, const QString& indexMem, Memory& mem, Register& reg);
+    static void store(const QString& indexReg, const QString& indexMem, Memory& mem, Register& reg);
     static void move(const QString& indexReg1, const QString& indexReg2, Register& reg);
-    static void jump(const QString& indexReg, int NewProgramCounter, Register& reg);
+    static void jump(const QString& indexReg, const QString& indexMem, Register& reg, Memory&mem, int programCounter);
     static void halt();
 };
 
